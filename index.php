4a2fcd70b864ffdfa1d074eb16123d498ec0097b
Kos7xm <kos7xm@gmail.com>
2021-03-23 16:15:47 +0200

    add variables

--- index.php
+++ index.php
@@ -3,43 +3,46 @@ $subid1 = $_GET['camp'];
 $subid2 = $_GET['adset'];
 $subid3 = $_GET['ban'];
 $subid4 = $_GET['s4'];
+$fbclid = $_GET['fbclid'];
 ?>
 <!DOCTYPE html>
 <html lang="pl">
 <head>
 
-<meta charset="utf-8">
-<meta name="viewport" content="width=device-width">
-	<title>LATARKA TAKTYCZNA ATOMIC BEAM
-</title>
-	
-	<link rel="shortcut icon" href="favicon.ico"> 
-	<link href="css.css?family=Open+Sans:400,700&amp;subset=cyrillic" rel="stylesheet">
-	<link rel="stylesheet" href="css/settings.css">
-	<link rel="stylesheet" href="css/timer.css">
-	<link rel="stylesheet" href="css/slick.css">
-	<link rel="stylesheet" href="css/style.css">	
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width">
+    <title>LATARKA TAKTYCZNA ATOMIC BEAM
+    </title>
+
+    <link rel="shortcut icon" href="favicon.ico">
+    <link href="css.css?family=Open+Sans:400,700&amp;subset=cyrillic" rel="stylesheet">
+    <link rel="stylesheet" href="css/settings.css">
+    <link rel="stylesheet" href="css/timer.css">
+    <link rel="stylesheet" href="css/slick.css">
+    <link rel="stylesheet" href="css/style.css">
 
     <!-- Facebook Pixel Code -->
 
 
+    <!-- End Facebook Pixel Code -->
 
-<!-- End Facebook Pixel Code -->
 
+    <script src="ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+    <script type="text/javascript" src="https://mldata.pro/65480/prices.js?product_ids=15151&{mleads_url}"></script>
 
-<script src="ajax/libs/jquery/2.1.3/jquery.min.js"></script>	
-<script type="text/javascript" src="https://mldata.pro/65480/prices.js?product_ids=15151&{mleads_url}"></script>
-
-<script type="text/javascript" src="subid.js"></script></head>
+    <script type="text/javascript" src="subid.js"></script>
+</head>
 <body>
+
 <section class="block1">
     <div class="wrap">
-        <h1>LATARKA TAKTYCZNA 
- <span>ATOMIC BEAM</span></h1>
-        <h2>ULTRA JASNE, 
- <span>ODPORNA NA WSTRZĄSY LATARKA TAKTYCZNA</span></h2>
+        <h1>LATARKA TAKTYCZNA
+            <span>ATOMIC BEAM</span></h1>
+        <h2>ULTRA JASNE,
+            <span>ODPORNA NA WSTRZĄSY LATARKA TAKTYCZNA</span></h2>
         <div class="sale">Promocja!
- <p>-50%</p> Zniżka</div>
+            <p>-50%</p> Zniżka
+        </div>
         <!--<div class="gift">В подарок часы <span>breitling navitimer</span></div>-->
         <ul class="plus">
             <li><p>5 trybów </p> pracy</li>
@@ -49,7 +52,7 @@ $subid4 = $_GET['s4'];
         </ul>
         <div class="formbox">
             <p class="count-title">Do końca promocji pozostało:
-</p>   
+            </p>
             <div class="countbox clearfix"></div>
             <div class="price clearfix">
                 <div class="old-cost">
@@ -60,79 +63,90 @@ $subid4 = $_GET['s4'];
                 </div>
                 <div class="new-cost">
                     <p><b class="price_only15151"></b>
-                    <b class="price_currency"></b></p>
+                        <b class="price_currency"></b></p>
                 </div>
             </div>
-            <form class="main-order-form m1-form" action="sendleads.php?<?php echo "subid1=$subid1&subid2=$subid2&subid3=$subid3&subid4=$subid4" ?>" method="post" onsubmit="return cartTelCheck('phone', true, this, event); ">
-                 <input class="field" type="text" name="client" placeholder="Imię Nazwisko" required="">
-            <input class="field mask" type="text" id="phone" name="tel" placeholder="Numer telefonu" required="">
+            <form class="main-order-form m1-form"
+                  action="sendleads.php?<?php echo "subid1=$subid1&subid2=$subid2&subid3=$subid3&subid4=$subid4&fbclid=$fbclid" ?>" method="get"
+                  onsubmit="return cartTelCheck('phone', true, this, event); ">
+                <input class="field" type="text" name="client" placeholder="Imię Nazwisko" required="">
+                <input class="field mask" type="text" id="phone" name="tel" placeholder="Numer telefonu" required="">
                 <button type="submit" class="button-m">ZAMÓW ZE ZNIŻKĄ
-</button>
-            
+                </button>
 
-</form>
-            <p class="stock">Pozostało  <span>9</span>  paczek z rabatem</p>
+
+            </form>
+            <p class="stock">Pozostało <span>9</span> paczek z rabatem</p>
         </div>
     </div>
-</section>  
-<!-- michael-k90 -->  
+</section>
+<!-- michael-k90 -->
 <section class="block2">
     <div class="wrap">
         <ul class="info clearfix">
             <li><p>Wysyłka kurierem w ciągu 2-3 dni roboczych
-</p></li>
-            <li><p>Płatność przy odbiorze 
-</p></li>
+                </p></li>
+            <li><p>Płatność przy odbiorze
+                </p></li>
             <li><p>Zwrot w ciągu 14 dni od daty otrzymania
-</p></li>
+                </p></li>
         </ul>
     </div>
-</section> 
+</section>
 <section class="block3">
     <div class="wrap">
-        <h2 class="title yl">WIĘCEJ O 
-<span>LATARNI</span></h2>
+        <h2 class="title yl">WIĘCEJ O
+            <span>LATARNI</span></h2>
         <div class="video-container">
-           <img src="img/1.gif" alt="тактический фонарь атомный луч">
+            <img src="img/1.gif" alt="тактический фонарь атомный луч">
         </div>
-        <p class="center-justified">Ultra jasna latarka taktyczna Atomic Beam nigdy nie pozwoli Ci pozostać w ciemności bez względu na warunki. Ze względu na niewiarygodnie jasną diodę LED jest uważana za jedną z najpotężniejszych latarni na świecie! Ma funkcję ustawiania ostrości od potężnej wiązki kierunkowej do Super jasnego reflektora, a także ma 5 trybów świecenia: mocny, średni, ekonomiczny, stroboskop i SOS. Sztywna konstrukcja sprawia, że latarnia jest praktycznie niezniszczalna, a użycie aluminium jest łatwe, dzięki czemu zawsze możesz ją zabrać ze sobą!
-</p>
+        <p class="center-justified">Ultra jasna latarka taktyczna Atomic Beam nigdy nie pozwoli Ci pozostać w ciemności
+            bez względu na warunki. Ze względu na niewiarygodnie jasną diodę LED jest uważana za jedną z
+            najpotężniejszych latarni na świecie! Ma funkcję ustawiania ostrości od potężnej wiązki kierunkowej do Super
+            jasnego reflektora, a także ma 5 trybów świecenia: mocny, średni, ekonomiczny, stroboskop i SOS. Sztywna
+            konstrukcja sprawia, że latarnia jest praktycznie niezniszczalna, a użycie aluminium jest łatwe, dzięki
+            czemu zawsze możesz ją zabrać ze sobą!
+        </p>
     </div>
-</section>    
+</section>
 <section class="block4">
     <div class="wrap">
         <ul class="demo">
-            <li><img src="img/d1.jpg" alt="тактический фонарь атомный луч"><h3>latarka taktyczna Atomic Beam
-WYTRZYMUJE MRÓZ W LODZIE
-</h3></li>
-            <li><img src="img/d2.jpg" alt="тактический фонарь атомный луч"><h3> latarka taktyczna Atomic Beam
-DZIAŁA NAWET WE WRZĄCYM OLEJU
-</h3></li>
-            <li><img src="img/d3.jpg" alt="тактический фонарь атомный луч"><h3> latarka taktyczna Atomic Beam
-WYTRZYMUJE UDERZENIE MŁOTEM
-</h3></li>
-            <li><img src="img/d4.jpg" alt="тактический фонарь атомный луч"><h3> latarka taktyczna Atomic Beam
-DO 40 RAZY MOCNIEJSZY NIŻ ZWYKŁA LATARNIA
+            <li><img src="img/d1.jpg" alt="тактический фонарь атомный луч">
+                <h3>latarka taktyczna Atomic Beam
+                    WYTRZYMUJE MRÓZ W LODZIE
+                </h3></li>
+            <li><img src="img/d2.jpg" alt="тактический фонарь атомный луч">
+                <h3> latarka taktyczna Atomic Beam
+                    DZIAŁA NAWET WE WRZĄCYM OLEJU
+                </h3></li>
+            <li><img src="img/d3.jpg" alt="тактический фонарь атомный луч">
+                <h3> latarka taktyczna Atomic Beam
+                    WYTRZYMUJE UDERZENIE MŁOTEM
+                </h3></li>
+            <li><img src="img/d4.jpg" alt="тактический фонарь атомный луч">
+                <h3> latarka taktyczna Atomic Beam
+                    DO 40 RAZY MOCNIEJSZY NIŻ ZWYKŁA LATARNIA
 
-</h3></li>
+                </h3></li>
         </ul>
     </div>
-</section>    
+</section>
 <section class="block5">
     <div class="wrap">
         <h2 class="title yl">TRYBY PRACY <span>LATARKI </span></h2>
         <ul class="job">
             <li><h3>POTĘŻNY
-</h3></li>
+                </h3></li>
             <li><h3>ŚREDNI
-</h3></li>
+                </h3></li>
             <li><h3>INTENDENT
-</h3></li>
+                </h3></li>
             <li><h3>Stroboskop</h3></li>
             <li><h3>s.o.s.</h3></li>
         </ul>
     </div>
-</section>                
+</section>
 <!-- owmen17 -->
 <!--<section class="block6">
     <div class="wrap">
@@ -145,7 +159,7 @@ DO 40 RAZY MOCNIEJSZY NIŻ ZWYKŁA LATARNIA
 </section> -->
 <section class="block9">
     <div class="wrap">
-        <h2 class="title yl">MOŻLIWOŚCI  <span>ZASTOSOWANIA</span></h2>
+        <h2 class="title yl">MOŻLIWOŚCI <span>ZASTOSOWANIA</span></h2>
         <ul class="cap">
             <li><h3>ŁOWIENIE RYB</h3></li>
             <li><h3>NA POLOWANIU</h3></li>
@@ -155,7 +169,7 @@ DO 40 RAZY MOCNIEJSZY NIŻ ZWYKŁA LATARNIA
             <li><h3>PODCZAS NAPRAWY</h3></li>
             <li><h3>W KRAJU</h3></li>
             <li><h3>W GOSPODARSTWIE
-</h3></li>
+                </h3></li>
         </ul>
     </div>
 </section>
@@ -186,19 +200,19 @@ DO 40 RAZY MOCNIEJSZY NIŻ ZWYKŁA LATARNIA
 <section class="block7">
     <div class="wrap">
         <h2 class="title yl">JAK ZŁOŻYĆ ZAMÓWIENIE?
-</h2>
+        </h2>
         <ul class="ship">
             <li><p>Składasz wniosek <br>poprzez formularz <br>zamówienia na stronie
- </p></li>
+                </p></li>
             <li><p>Oddzwonimy po szczegóły
-</p></li>
+                </p></li>
             <li><p>Wysyłka kurierem <br>w ciągu 2-3 dni <br>roboczych
-</p></li>
-            <li><p>Płatność przy odbiorze 
-</p></li>
+                </p></li>
+            <li><p>Płatność przy odbiorze
+                </p></li>
         </ul>
         <a href="#buy" class="button-m">ZAMÓW ZE ZNIŻKĄ
-</a>
+        </a>
     </div>
 </section>
 <!-- after_qbici -->
@@ -209,22 +223,32 @@ DO 40 RAZY MOCNIEJSZY NIŻ ZWYKŁA LATARNIA
             <div>
                 <div class="rev">
                     <h3>Adam</h3>
-                    <p>Jestem myśliwym z wieloletnim doświadczeniem i doskonale rozumiem znaczenie czegoś takiego jak Latarnia. Kiedyś moi przyjaciele otrzymali bu-shny Army i używałem go przez 5 lat, ale potem straciłem go na Kamczatce. Wczesną wiosną kupił tutaj i był mile zaskoczony. Latarnia okazała się lżejsza i bardziej kompaktowa niż moja stara, a świeci i jest o rząd wielkości lepsza niż poprzednia.
-</p>
+                    <p>Jestem myśliwym z wieloletnim doświadczeniem i doskonale rozumiem znaczenie czegoś takiego jak
+                        Latarnia. Kiedyś moi przyjaciele otrzymali bu-shny Army i używałem go przez 5 lat, ale potem
+                        straciłem go na Kamczatce. Wczesną wiosną kupił tutaj i był mile zaskoczony. Latarnia okazała
+                        się lżejsza i bardziej kompaktowa niż moja stara, a świeci i jest o rząd wielkości lepsza niż
+                        poprzednia.
+                    </p>
                 </div>
             </div>
             <div>
                 <div class="rev r2">
                     <h3>Kasper</h3>
-                    <p>Moja żona i ja lubimy jeździć wieczorami na rowerach i często wychodzimy do lasu na spacer. Do wygodnej, małej podróży potrzebna była latarka, aby oświetlić sobie drogę i nie zgubić się. Jak tylko znalazłem się na tej stronie, zamówiłem bez wahania 2 sztuki, dla siebie i dla żony. Cechy są odpowiednie, wygląd jest piękny, latarnie są niezawodne.
-</p>
+                    <p>Moja żona i ja lubimy jeździć wieczorami na rowerach i często wychodzimy do lasu na spacer. Do
+                        wygodnej, małej podróży potrzebna była latarka, aby oświetlić sobie drogę i nie zgubić się. Jak
+                        tylko znalazłem się na tej stronie, zamówiłem bez wahania 2 sztuki, dla siebie i dla żony. Cechy
+                        są odpowiednie, wygląd jest piękny, latarnie są niezawodne.
+                    </p>
                 </div>
             </div>
             <div>
                 <div class="rev r3">
                     <h3>Bartek</h3>
-                    <p>Uwielbiam łowić ryby, szczególnie w nocy, kiedy jest już cisza i nic nie rozprasza. To jedyny model, któremu ufam, ponieważ wcześniej od deszczu lub upadku wszystkie światła pękały, przez długi czas nie wystarczały. Zadowolony z jakości i ceny i używam go nie tylko na łowisku, ale także podczas prac domowych i polowań.
-</p>
+                    <p>Uwielbiam łowić ryby, szczególnie w nocy, kiedy jest już cisza i nic nie rozprasza. To jedyny
+                        model, któremu ufam, ponieważ wcześniej od deszczu lub upadku wszystkie światła pękały, przez
+                        długi czas nie wystarczały. Zadowolony z jakości i ceny i używam go nie tylko na łowisku, ale
+                        także podczas prac domowych i polowań.
+                    </p>
                 </div>
             </div>
         </div>
@@ -232,21 +256,22 @@ DO 40 RAZY MOCNIEJSZY NIŻ ZWYKŁA LATARNIA
 </section>
 <section class="block1 end">
     <div class="wrap">
-        <h1>LATARKA TAKTYCZNA 
- <span>ATOMIC BEAM</span></h1>
-        <h2>ULTRA JASNE, 
- <span>ODPORNA NA WSTRZĄSY LATARKA TAKTYCZNA</span></h2>
+        <h1>LATARKA TAKTYCZNA
+            <span>ATOMIC BEAM</span></h1>
+        <h2>ULTRA JASNE,
+            <span>ODPORNA NA WSTRZĄSY LATARKA TAKTYCZNA</span></h2>
         <div class="sale">Promocja!
- <p>-50%</p> Zniżka</div>
+            <p>-50%</p> Zniżka
+        </div>
         <!--<div class="gift">В подарок часы <span>breitling navitimer</span></div>-->
-          <ul class="plus">
+        <ul class="plus">
             <li><p>5 trybów </p> pracy</li>
             <li><p>Lekki i </p> kompaktowy</li>
             <li><p>Aluminiowa </p> obudowa</li>
             <li><p>Niezawodny </p> przycisk włączania</li>
         </ul>
         <div class="formbox">
-            <p class="count-title">Do końca promocji pozostało:</p>   
+            <p class="count-title">Do końca promocji pozostało:</p>
             <div class="countbox clearfix"></div>
             <div class="price clearfix">
                 <div class="old-cost">
@@ -257,61 +282,42 @@ DO 40 RAZY MOCNIEJSZY NIŻ ZWYKŁA LATARNIA
                 </div>
                 <div class="new-cost">
                     <p><b class="price_only15151"></b>
-                    <b class="price_currency"></b></p>
+                        <b class="price_currency"></b></p>
                 </div>
             </div>
-            <form id="buy" class="main-order-form m1-form" action="sendleads.php?<?php echo "subid1=$subid1&subid2=$subid2&subid3=$subid3&subid4=$subid4" ?>" method="post" onsubmit="return cartTelCheck('phone2', true, this, event); ">
-               <input class="field" type="text" name="client" placeholder="Imię Nazwisko" required="">
-            <input class="field mask" type="text" id="phone2" name="tel" placeholder="Numer telefonu" required="">
+            <form id="buy" class="main-order-form m1-form"
+                  action="sendleads.php?<?php echo "subid1=$subid1&subid2=$subid2&subid3=$subid3&subid4=$subid4&fbclid=$fbclid" ?>"
+                  method="get" onsubmit="return cartTelCheck('phone2', true, this, event); ">
+                <input class="field" type="text" name="client" placeholder="Imię Nazwisko" required="">
+                <input class="field mask" type="text" id="phone2" name="tel" placeholder="Numer telefonu" required="">
                 <button type="submit" class="button-m">ZAMÓW ZE ZNIŻKĄ
-</button>
-            
-            
+                </button>
 
-</form>
-              <p class="stock">Pozostało  <span>9</span>  paczek z rabatem</p>
+
+            </form>
+            <p class="stock">Pozostało <span>9</span> paczek z rabatem</p>
         </div>
     </div>
-</section> 
+</section>
 <!-- michael-k90 -->
 <footer class="footer">
     <div class="wrap">
-    	
+
     </div>
 </footer>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 <script src="ajax/libs/html5shiv/3.7.3/html5shiv.min.js"></script>
 <script src="js/count.js"></script>
 <script src="js/slick.min.js"></script>
 <!-- <script src="js/main.js"></script>
- --><script src="assets_pages/land/js/youtube/previewYouTube.js"></script>
- <script src="js/jquery.js" type="text/javascript"></script>
+ -->
+<script src="assets_pages/land/js/youtube/previewYouTube.js"></script>
+<script src="js/jquery.js" type="text/javascript"></script>
 <script src="js/scripts.js"></script>
 
 
 <script src="js/scripts.js" type="text/javascript"></script>
-
+<?php echo '<img src="https://nawva.abtr4all.com/c/5d063098b96d70ae?camp='.$subid1.'&adset='.$subid2.'&ban='.$subid3.'&s4='.$subid4.'&fbclid='.$fbclid.'">';?>
 </body>
 </html>
